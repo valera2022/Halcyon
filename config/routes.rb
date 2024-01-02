2ecc65c0bf178eaceba57a8a7c0840c4bd28bee0
GeralmyUrena <geralmyuri11@gmail.com>
2024-01-01 22:29:32 -0500

    root to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#show"
   post "/login", to:"sessions#create"
   delete "/logout", to:"sessions#destroy"
   post "/classes", to:"courses#create"
