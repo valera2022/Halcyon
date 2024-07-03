b340ecd17f9e68cd64f772a6930d732258f972f2
GeralmyUrena <geralmyuri11@gmail.com>
2024-07-02 23:29:41 -0400

    build in rails public

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -5,8 +5,8 @@ class FallbackController < ActionController::API
     def index
       # React app index page
       # render file: 'client/halcyon_front/public/index.html'
-      # render file: Rails.root.join('public', 'index.html')
-      render file: Rails.root.join('client', 'halcyon_front', 'build', 'index.html')
+      render file: Rails.root.join('public', 'index.html')
+      # render file: Rails.root.join('client', 'halcyon_front', 'build', 'index.html')
       # render file: Rails.root.join('public', 'index.html')
     #   render file: './client/public/index.html'
     end
